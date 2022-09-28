ba1fe5b5d10da6b0f36320c4e352ef6a70d398f2
Baran EKREM <baranekrem@outlook.com>
2022-09-28 06:25:48 +0300

    Create All

---
+++ inc/main.h
@@ -0,0 +1,28 @@
+/**==============================================================================
+ *  @file     	main.h
+ *  @author   	Baran EKREM
+ *  @email    	baranekrem@outlook.com
+ *  @WebSite    www.baranekrem.net
+ *  @compiler 	GCC or ARM MDK
+ *  @hardware 	Nuvoton NuMicro
+ *  @version  	v1.0
+ *  @brief      Basic defines and includes
+==============================================================================*/
+#ifndef MAIN_H
+#define MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "NuMicro.h"
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif // MAIN_H Included
